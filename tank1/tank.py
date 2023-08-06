db71f107ed3d62bcd9c5bdb8e03c54241dd23968
igorferrati <ferrati.igor@gmail.com>
2023-08-06 20:01:43 -0300

    Aplicando a1 - print corrigidos

--- tank1/tank.py
+++ tank1/tank.py
@@ -36,11 +36,14 @@ class Tank(object):
 def create_tank():
     tanks = {}
     while True:
-        qtd = int(input('Quantos tanques vão jogar?[2/10]: '))
-        if qtd < 2 or qtd > 10:
-            print('Quantidade invalida!!, digite de 2 a jogadores!')
-        else:
-            break
+        try:
+            qtd = int(input('Quantos tanques vão jogar?[2/10]: '))
+            if not 2 <= qtd <= 10:
+                print('Quantidade inválida! Digite um valor entre 2 e 10.')
+            else:
+                break
+        except ValueError:
+            print('Entrada inválida! Digite um número inteiro válido.')
 
     for i in range(qtd):
         name = input(f'Nome do {i+1}° TANQUE: ')
@@ -53,28 +56,33 @@ def player_info():
     for tank in tanks.values():
         print(tank)
 
-print(f'{"*" * 25} Batalha de Tanques {"*" * 25}\n')
-print(f'{"_" * 18} Cada jogador deve criar um tanque! {"_" * 18}\n')
+
+#game
+print(f'{"*" * 25} Batalha de Tanques {"*" * 25}')
+print(f'Cada jogador deve criar um tanque!!!\n')
 
 tanks = create_tank()
 players = list(tanks.keys())
 
 while len(tanks) > 1:
+    round = 0
     current_player = random.choice(players)
     tank = tanks[current_player]
 
     if not tank.alive:
         continue
 
-    print(f'{"_" * 25} Informações dos Jogadores ! {"_" * 25}\n')
+    print(f'{"_" * 70}\n')
+    print(f'Status dos jogadores:')
+    time.sleep(1)
     player_info()
-    print(f'{"_" * 50}\n')
+    print(f' \n')
 
-    print(len(tanks))
-    print("Sorteando...\n")
+    print(f'{"*" *30} Round: {round} {"*" *30}')
+    print("Sorteando...")
     time.sleep(2)
 
-    print(f"{tank} sorteado! fará um ataque!\n")
+    print(f"{tank.name} sorteado! fará um ataque!!")
 
     enemy_players = {k: tanks[k] for k in tanks if k != current_player and tanks[k].alive}
     if not enemy_players:
@@ -93,15 +101,15 @@ while len(tanks) > 1:
 
     target = enemy_players[target_key]
 
-    print(f"{tank} ATACA {target}")
+    print(f"{tank.name} ATACA {target.name}")
     tank.fire_at(target)
-    print(f'{"-" * 70}\n')
 
     if not target.alive:
-        print(f" {target} fora do jogo!")
+        print(f"{target} fora do jogo!")
         tanks.pop(target_key)
         players.remove(target_key)
-        print(f'{"-" * 70}\n')
+    
+    round += 1
 
 winner = tanks[list(tanks.keys())[0]]
 print(f"VENCEDOR: {winner.name}")
