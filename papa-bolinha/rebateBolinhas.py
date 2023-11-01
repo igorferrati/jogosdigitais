8d7e3ea2eb211714e0862187facff63cf063d88b
igorferrati <igorferrati93@gmail.com>
2023-10-31 21:21:20 -0300

    add aplicando A7

--- papa-bolinha/rebateBolinhas.py
+++ papa-bolinha/rebateBolinhas.py
@@ -25,7 +25,7 @@ font = pygame.font.SysFont('sans',40)
 placar = 0
 
 # Posição X e Y do circulo 
-posicaoPapaBolinhas = [400, 300]
+posicaoPapaBolinhas = [400, 500]
 
 # Armazena num vetor a Velocidade de movimentacao do circulo 
 velocidadePapaBolinhas = [5, 5]
@@ -36,6 +36,9 @@ criar = True
 # Variáveis de posição do círculo vermelho
 X_vermelho = 0
 Y_vermelho = 0
+dx_vermelho = randint(-5,5)
+dy_vermelho = 5
+
 
 # Variável para contagem de tempo, utilizado para controlar a velocidade de quadros (de atualizações da tela)
 clock = pygame.time.Clock()
@@ -82,25 +85,32 @@ while True:
         criar = False
 
     # Velocidade de queda do círculo Vermelho
-    Y_vermelho += 5
+    Y_vermelho += dy_vermelho
+    X_vermelho += dx_vermelho
 
     # Valores da bola vermelha é atribuido 
     posicaoBolasVermelhas = [X_vermelho,Y_vermelho]
    
-    # Desenha o círculo vermelh
+    # Desenha o círculo vermelho
     pygame.draw.circle(screen, random_color, posicaoBolasVermelhas, 10)
 
-    # Se o círculo vermelho ultapassar a  tela ela é reiniciada
+    # ultrapassar tela cria um novo e rebatendo limites da tela
     if Y_vermelho > 600:
         criar = True
+    if Y_vermelho <= 9:
+        dy_vermelho = -dy_vermelho #inverte
+    if X_vermelho > 790 or X_vermelho < 9:
+        dx_vermelho = -dx_vermelho
 
     # Se o papa bolinhas encostar no círculo vermelho o círculo vermelho é reiniciado
     # CB: Círculo Branco    CV: Círculo Vermelho 
     if (posicaoPapaBolinhas[1] + 20 >= Y_vermelho - 10  and posicaoPapaBolinhas[1] - 20 <= Y_vermelho + 10) and (posicaoPapaBolinhas[0] +20  >= X_vermelho - 10 and posicaoPapaBolinhas[0] - 20 <= X_vermelho + 20): 
-        criar = True
+        dy_vermelho = -dy_vermelho
+        dx_vermelho = randint(-5, 5)
         placar = placar+1
         pygame.mixer.music.load("/home/igor/projects/jogosdigitais/papa-bolinha/som/catch.mp3")
         pygame.mixer.music.play(0)
+
         #muda cor
         for i in range(3):
             random_color[i] = randint(0, 255)
